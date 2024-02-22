702deeaa14216cb818804b0f1207220968cfb84d
Tr14 <xuantrucx111@gmail.com>
2024-02-22 17:10:30 +0700

    new webhook

--- src/plugins/netcore/server/controllers/netcore.js
+++ src/plugins/netcore/server/controllers/netcore.js
@@ -5,5 +5,18 @@ module.exports = {
         console.log(ctx.request.body);
         //console.log(ctx.request.header);
         ctx.body = "AKADIGITAL"
+
+        const entry = await strapi.db.query('api::netcore.netcorelead').create({
+            data: {
+                Email: ctx.request.body.data.voolatechsmt[0].att_params.EMAIL,
+                Mobile: "",
+                Source: ctx.request.body.data.voolatechsmt[0].att_params.SOURCE,
+                Order_ID: ctx.request.body.data.voolatechsmt[0].att_params.ORDER_ID,
+                Full_Name: ctx.request.body.data.voolatechsmt[0].att_params.FULL_NAME,
+                Journey_Name: ctx.request.body.data.voolatechsmt[0].custom_params.Journey_Name,
+                List_ID: ctx.request.body.data.voolatechsmt[0].custom_params.List_ID,
+                Is_Get: false
+            },
+        });
     }
 };
