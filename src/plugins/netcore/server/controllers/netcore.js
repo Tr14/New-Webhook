97d093cf7c27759957de43b5c0e9f2e6ba52b5dc
Tr14 <xuantrucx111@gmail.com>
2024-02-22 15:10:46 +0700

    new webhook

--- src/plugins/netcore/server/controllers/netcore.js
+++ src/plugins/netcore/server/controllers/netcore.js
@@ -3,7 +3,7 @@
 module.exports = {
     async webhook(ctx) {
         console.log(ctx.request.body);
-        console.log(ctx.request.header);
+        //console.log(ctx.request.header);
         ctx.body = "AKADIGITAL"
     }
 };
