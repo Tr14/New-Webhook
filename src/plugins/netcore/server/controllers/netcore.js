49e1cbba74f0ef64005cb608ba44c37045d4af4b
Tr14 <xuantrucx111@gmail.com>
2024-02-23 11:43:39 +0700

    new webhook

--- src/plugins/netcore/server/controllers/netcore.js
+++ src/plugins/netcore/server/controllers/netcore.js
@@ -9,31 +9,10 @@ module.exports = {
         if (Object.keys(ctx.request.body).length === 0) {
             console.log("Null");
             ctx.body = "Body is null" 
-        } else if (typeof(ctx.request.body) == 'string'){
-            console.log("String");
-            var obj1 = JSON.parse(ctx.request.body);
-            console.log(obj1);
-            var string1 = obj1.data.replace(/'/g, "\"");
-            console.log(string1);
-            var result = JSON.parse(string1);
-            console.log(result);
-
-            const entry = await strapi.db.query('plugin::netcore.netcorelead').create({
-                data: {
-                    Email: result.voolatechsmt[0].att_params.EMAIL,
-                    Mobile: "",
-                    Source: result.voolatechsmt[0].att_params.SOURCE,
-                    Order_ID: result.voolatechsmt[0].att_params.ORDER_ID,
-                    Full_Name: result.voolatechsmt[0].att_params.FULL_NAME,
-                    Journey_Name: result.voolatechsmt[0].custom_params.Journey_Name,
-                    List_ID: result.voolatechsmt[0].custom_params.List_ID,
-                    Is_Get: false
-                },
-            });
-            ctx.body = "Receive string data from body"
-        } 
+        }
         else {
             console.log("Object");
+            /*
             var string1 = ctx.request.body.data.replace(/'/g, "\"");
             var result = JSON.parse(string1);
 
@@ -50,6 +29,7 @@ module.exports = {
                 },
             });
             ctx.body = "Receive object data from body"
+            */
         }   
     }
 };
