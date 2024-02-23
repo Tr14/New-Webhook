f66c7b9216d7fb1099c66b71d2737329c4d1d2f6
Tr14 <xuantrucx111@gmail.com>
2024-02-23 11:09:36 +0700

    new webhook

--- src/plugins/netcore/server/controllers/netcore.js
+++ src/plugins/netcore/server/controllers/netcore.js
@@ -4,22 +4,26 @@ module.exports = {
     async webhook(ctx) {
         console.log(ctx.request.body);
         //console.log(ctx.request.header);
-        ctx.body = "AKADIGITAL"
 
-        let data_body = ctx.request.body.data;
-        let result = JSON.parse(data_body);
+        if (ctx.request.body != null) {
+            let data_body = ctx.request.body.data;
+            let result = JSON.parse(data_body);
 
-        const entry = await strapi.db.query('plugin::netcore.netcorelead').create({
-            data: {
-                Email: result.voolatechsmt[0].att_params.EMAIL,
-                Mobile: "",
-                Source: result.voolatechsmt[0].att_params.SOURCE,
-                Order_ID: result.voolatechsmt[0].att_params.ORDER_ID,
-                Full_Name: result.voolatechsmt[0].att_params.FULL_NAME,
-                Journey_Name: result.voolatechsmt[0].custom_params.Journey_Name,
-                List_ID: result.voolatechsmt[0].custom_params.List_ID,
-                Is_Get: false
-            },
-        });
+            const entry = await strapi.db.query('plugin::netcore.netcorelead').create({
+                data: {
+                    Email: result.voolatechsmt[0].att_params.EMAIL,
+                    Mobile: "",
+                    Source: result.voolatechsmt[0].att_params.SOURCE,
+                    Order_ID: result.voolatechsmt[0].att_params.ORDER_ID,
+                    Full_Name: result.voolatechsmt[0].att_params.FULL_NAME,
+                    Journey_Name: result.voolatechsmt[0].custom_params.Journey_Name,
+                    List_ID: result.voolatechsmt[0].custom_params.List_ID,
+                    Is_Get: false
+                },
+            });
+            ctx.body = "Receive data from body"
+        } else {
+            ctx.body = "Body is null"
+        }   
     }
 };
